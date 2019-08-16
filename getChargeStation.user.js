48932c2f94f5efa0e9b554e70e62069e6be5f014
musclehunter <44521623+musclehunter@users.noreply.github.com>
2019-08-16 15:17:46 +0900

    Update getChargeStation.user.js

--- getChargeStation.user.js
+++ getChargeStation.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Aden Charge Station
 // @namespace    https://github.com/musclehunter
-// @version      0.1
+// @version      0.2
 // @description  easy login and receive charge station in Lineage2 JP classic Aden server
 // @author       MuscleHunter
 // @match        https://*.ncsoft.jp/*
